03ca62621b39c8b7fe32137f24eeafcbe221bdea
kalu john <kalu.johnagu@gmail.com>
2022-08-10 08:16:41 +0100

    setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const App = () => {
+  return (
+    <div>
+      <h2>Portofolio</h2>
+    </div>
+  )
+}
+
+export default App
